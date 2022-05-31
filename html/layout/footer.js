23a45abb37e2123de06ba590a11728fc9b2dea79
VCS <vincentcubeP@gmail.com>
2022-05-31 12:12:10 +0900

    https://github.com/vincentcubeP/airbnb.git

---
+++ html/layout/footer.js
@@ -0,0 +1,31 @@
+var str =
+`<div id="footer">
+    <div class="checkMap">
+        <button><p>지도 표시하기</p></button>
+    </div>
+    <div class="footer">
+        <ul class="footerInfo">
+            <li>@2002 Airbnb,Inc.</li>
+            <li>·</li>
+            <li>개인정보 처리방침</li>
+            <li>·</li>
+            <li>이용약관</li>
+            <li>·</li>
+            <li>사이트맵</li>
+            <li>·</li>
+            <li>한국의 변경된 환불 정책</li>
+            <li>·</li>
+            <li>회사 세부정보</li>
+        </ul>
+        <ul class="footerSide">
+            <li></li>
+            <li>한국어(KR)</li>
+            <li>KRW</li>
+            <li>지원 및 참고 자료</li>
+            <li></li>
+        </ul>
+    </div>
+</div>
+`;
+
+document.write(str);
