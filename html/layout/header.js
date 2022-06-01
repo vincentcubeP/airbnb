eaa7b3e5465c0a88c74bb92110da0df75ce3217d
VCS <vincentcubeP@gmail.com>
2022-06-01 11:13:45 +0900

    header/footer

---
+++ html/layout/header.js
@@ -0,0 +1,19 @@
+var str =
+`<div class="header">
+<div class="headerTop">
+    <div class="headerTopBox">
+
+    </div>
+</div>
+<div class="headerMenu">
+    <div class="menu">
+        <div class="menuSlide"></div>
+        <div class="menuFilter">
+            <div class="menuFilterBox"></div>
+        </div>
+    </div>
+</div>
+</div>
+`;
+
+document.write(str);
