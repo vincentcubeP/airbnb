3d04762b45647a05a68540386979bafa4d6b1577
VCS <vincentcubeP@gmail.com>
2022-06-17 11:15:44 +0900

    220617

--- js/slide.js
+++ js/slide.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
     var mySwiper = new Swiper ('.swiper-container',{
         direction: 'horizontal',
-        width: 200,        
+        width: 80,        
         loop: false,
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
-        },
-    })
+        }
+    })    
 });
