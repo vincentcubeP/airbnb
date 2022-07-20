e8bf7bd925e17e2a6e1cd168ae2692c0bff50f79
VCS <vincentcubeP@gmail.com>
2022-07-20 23:44:15 +0900

    airbnb

--- js/index.js
+++ js/index.js
@@ -1,15 +1,29 @@
 /* modal popup */
-$(document).ready(function( $ ){
-    $(".header-login").on("click", function(event) { //팝업오픈 버튼 누르면
-    $("#modal").show(); //팝업 오픈
-    });
+// $(document).ready(function( $ ){
+//     $(".header-login").on("click", function(event) { //팝업오픈 버튼 누르면
+//     $("#modal").show(); //팝업 오픈
+//     });
     
-    $("body").on("click", function(event) {
-        if(event.target.className == 'header-login'){
-            $("#modal").hide(); //close버튼 이거나 뒷배경 클릭시 팝업 삭제
-        }
-    });
-});
+//     $("body").on("click", function(event) {
+//         if(event.target.className == 'header-login'){
+//             $("#modal").hide(); //close버튼 이거나 뒷배경 클릭시 팝업 삭제
+//         }
+//     });
+// });
+
+// 팝업 열기
+$(document).on("click", ".header-login", function (e){
+    var target = $(this).attr("href");
+    $(target).addClass("modal-show");
+  });
+  
+  // 외부영역 클릭 시 팝업 닫기
+  $(document).mouseup(function (e){
+    var LayerPopup = $(".modal-box");
+    if(LayerPopup.has(e.target).length === 0){
+      LayerPopup.removeClass("modal-show");
+    }
+  });
 
 
 /* popup */
