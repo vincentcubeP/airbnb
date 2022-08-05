ff84568dcb3cb1ab3d4c814df6e59fb40dd63732
VCS <vincentcubeP@gmail.com>
2022-08-05 11:01:08 +0900

    airbnb

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
+
+
 /* modal popup */
 // 팝업 열기
 $(document).on("click", ".header-login", function (e){
     var target = $(this).attr("href");
-    $(target).addClass("modal-show");
+    $(target).addClass("modal-show");    
 }); 
 // 외부영역 클릭 시 팝업 닫기
 $(document).mouseup(function (e){
@@ -17,16 +19,25 @@ $(document).mouseup(function (e){
 $(document).on("click", "#header-global", function (e){
     var target = $(this).attr("href");
     $(target).addClass("show-global");
+
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.add('scrollLock');
 });
 $(document).on("click", "#close-global", function (e){
     var target = $(this).attr("href");
     $(target).removeClass("show-global");
+
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.remove('scrollLock');
 });
 $(document).mouseup(function (e){
     var LayerPopup = $(".global-popup-window");
     if(LayerPopup.has(e.target).length === 0){
         $(".popup-global").removeClass("show-global");
     }
+
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.remove('scrollLock');
 });
 
 $(document).ready(function(){  
@@ -46,62 +57,98 @@ $(document).ready(function(){
 $(document).on("click", ".header-filter", function (e){
     var target = $(this).attr("href");
     $(target).addClass("show-filter");
+
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.add('scrollLock');
 });
 $(document).on("click", "#close-filter", function (e){
     var target = $(this).attr("href");
     $(target).removeClass("show-filter");
+
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.remove('scrollLock');
 });
 $(document).mouseup(function (e){
     var LayerPopup = $(".popup-filter-window");
     if(LayerPopup.has(e.target).length === 0){
         $(".popup-filter").removeClass("show-filter");
     }
+
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.remove('scrollLock');
 });
 
 /* popup */
 $(document).on("click", "#show", function (e){
     var target = $(this).attr("href");
     $(target).addClass("show");
+
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.add('scrollLock');
 });
 $(document).on("click", "#close", function (e){
     var target = $(this).attr("href");
     $(target).removeClass("show");
+
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.remove('scrollLock');
 });
 $(document).mouseup(function (e){
     var LayerPopup = $(".popup-window");
     if(LayerPopup.has(e.target).length === 0){
         $(".popup-lang").removeClass("show");
     }
+
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.remove('scrollLock');
 });
 
 $(document).on("click", "#show1", function (e){
     var target = $(this).attr("href");
     $(target).addClass("show1");
+
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.add('scrollLock');
 });
 $(document).on("click", "#close1", function (e){
     var target = $(this).attr("href");
     $(target).removeClass("show1");
+
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.remove('scrollLock');
 });
 $(document).mouseup(function (e){
     var LayerPopup = $(".popup-window");
     if(LayerPopup.has(e.target).length === 0){
         $(".popup-curr").removeClass("show1");
     }
+
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.remove('scrollLock');
 });
 
 $(document).on("click", "#show2", function (e){
     var target = $(this).attr("href");
     $(target).addClass("show2");
+
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.add('scrollLock');
 });
 $(document).on("click", "#close2", function (e){
     var target = $(this).attr("href");
     $(target).removeClass("show2");
+
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.remove('scrollLock');
 });
 $(document).mouseup(function (e){
     var LayerPopup = $(".popup-window");
     if(LayerPopup.has(e.target).length === 0){
         $(".popup-etc").removeClass("show2");
     }
+
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.remove('scrollLock');
 });
 
 /* swiper */
